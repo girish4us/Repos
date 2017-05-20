757652becd6bf12b630f8e9fb3b98e9d3a9143ef
root <root@ip-172-31-27-252.ap-south-1.compute.internal>
2017-05-20 07:30:35 -0400

    dsffsdF

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+file 'Hello.txt' do
+     action:create
+     content "Good morning to you"
+end
+
+service "httpd" do
+     action :start
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
